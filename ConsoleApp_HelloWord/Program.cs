f6e8e8234a404f69a8744f0d3eb67027d60159e4
sachin.vitthaldhopte@rrd.com <sachin.vitthaldhopte@rrd.com>
2025-01-02 19:55:26 +0530

    commit

---
+++ ConsoleApp_HelloWord/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp_HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int hello_counter = 0;
+            int hello_MsgCounter = 10;
+            while (hello_counter < hello_MsgCounter)
+            { 
+                hello_counter++;
+                Console.WriteLine( hello_counter.ToString() + ") Hello, World!");
+            }
+        }
+    }
+}
