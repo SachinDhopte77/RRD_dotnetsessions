926f0cfce865a24719da6dc9e141af274e08c2cc
SachinDhopte77 <sachin.vitthaldhopte@rrd.com>
2025-01-03 12:36:10 +0530

    day17day18

---
+++ day17/ConsoleApp_Hello_word/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp_Hello_word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int Hello_Counter = 10;
+            int Hello_Running_Counter = 0;
+            while (Hello_Running_Counter < Hello_Counter)
+            { 
+                Hello_Running_Counter++;
+                Console.WriteLine(Hello_Running_Counter.ToString() + " Hello, World!");
+            }
+        }
+    }
+}
---
+++ day18/HelloWord_CommandLineArgument/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWord_CommandLineArgument
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            { 
+            Console.WriteLine(args[i].ToUpper() + " Welcome to C#");
+            }
+            Console.WriteLine();
+
+            for (int i = args.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(args[i].ToUpper() + " Welcome to C#");
+            }
+
+        
+
+        }
+    }
+}
