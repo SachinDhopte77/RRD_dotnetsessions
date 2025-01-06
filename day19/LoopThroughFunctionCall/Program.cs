488f6506e611d9ff09da49f1d6e0238baa7ea81c
SachinDhopte77 <sachin.vitthaldhopte@rrd.com>
2025-01-06 19:23:58 +0530

    day19

---
+++ day19/LoopThroughFunctionCall/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] RRDfolks = GetvaluesinArray(); //Initialising Array
+
+        PrintNameForwardLoop(RRDfolks);//Redirecting to function: Returning Value in forward loop
+
+        PrintNameBackwardLoop(RRDfolks);//Redirecting to function: Returning Value in Backword loop
+    }
+    static string[] GetvaluesinArray()
+    {
+        string[] rrdfolks = { "Sachin", "Venkat", "Akhilesh" };
+        return rrdfolks;
+
+    }
+    static void PrintNameForwardLoop(string[] rrdfolks)
+    {
+        for (int i = 0; i < rrdfolks.Length; i++)
+        {
+            Console.WriteLine($"Hellow rrdfolks " + $"{rrdfolks[i]}");
+        }
+    }
+    static void PrintNameBackwardLoop(string[] rrdfolks)
+    {
+        for (int i = rrdfolks.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine($"Hellow rrdfolks " + $"{rrdfolks[i]}");
+        }
+    }
+}
---
+++ day19/RecursiveandOtherLabExcercise/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] RRDNameArray = { "Sachin", "Akhilesh", "Venkat", "Minu" };//initialising array
+
+        Console.WriteLine("Using Join Statement :");
+
+        LoopUsingJoin(RRDNameArray); //using Join statement function
+
+        Console.WriteLine("Using ToList For Each :");
+
+        ToListForEach(RRDNameArray); //Using ToList For Each
+
+        Console.WriteLine("Using Recursion function");//using Recusrsion Function
+
+        int RRDNameArraysize = RRDNameArray.Length - 1;
+
+        RecursiveFunction(RRDNameArraysize, RRDNameArray);
+
+    }
+    static void LoopUsingJoin(string[] RRDNameArray)
+    {
+        Console.WriteLine(string.Join(Environment.NewLine, RRDNameArray));
+    }
+
+    static void ToListForEach(string[] RRDNameArray)
+    {
+        RRDNameArray.ToList().ForEach(x => Console.WriteLine(x));
+    }
+
+    static int RecursiveFunction(int arraysize, string[] RRDNameArray)
+    {
+        if (arraysize >= 0)
+        {
+            Console.WriteLine(RRDNameArray[arraysize]);
+            RecursiveFunction(arraysize - 1, RRDNameArray);
+        }
+        return 1;
+    }
+
+}
