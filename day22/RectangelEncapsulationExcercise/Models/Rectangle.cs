8b05597460e0bb54c39cb0c408cdeba8eaf28b0d
SachinDhopte77 <sachin.vitthaldhopte@rrd.com>
2025-01-09 23:15:31 +0530

    day2-Part2

---
+++ day22/RectangelEncapsulationExcercise/Models/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangelEncapsulationExcercise.Models
+{
+    internal class Rectangle
+    {
+
+        private int _width;
+        private int _height;
+
+        private const int Minimum_Dimention = 10;
+        private const int Maximum_Dimention = 100;
+
+        public int CalculateArea()
+        {
+            return _width * _height;
+        }
+
+        public void SetWidth(int UserWidth)
+        {
+            _width = ValidateUserHeightWidth(UserWidth);
+        }
+        public int GetWidth() { return _width; }
+
+
+        public void SetHeight(int UserHeight)
+        {
+            _height = ValidateUserHeightWidth(UserHeight);
+        }
+
+        public int GetHeight() { return _height; }
+
+        private int ValidateUserHeightWidth(int Dimention)
+        {
+            if (Dimention < Minimum_Dimention) return Minimum_Dimention;
+            if (Dimention > Maximum_Dimention) return Maximum_Dimention;
+            return Dimention;
+        }
+
+
+    }
+}
---
+++ day22/RectangelEncapsulationExcercise/RectangleTest.cs
@@ -0,0 +1,63 @@
+﻿using RectangelEncapsulationExcercise.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RectangelEncapsulationExcercise
+{
+    internal class RectangleTest
+    {
+        static void Main()
+        {
+
+
+            Rectangle rectangle1 = new Rectangle();
+            rectangle1.SetWidth(9);
+            rectangle1.SetHeight(20);
+
+
+            Rectangle rectangle2 = new Rectangle();
+            rectangle2.SetWidth(25);
+            rectangle2.SetHeight(150);
+
+
+            Rectangle rectangle3 = new Rectangle();
+            rectangle3.SetWidth(5);
+            rectangle3.SetHeight(98);
+
+
+            Rectangle rectangle4 = new Rectangle();
+            rectangle4.SetWidth(25);
+            rectangle4.SetHeight(75);
+
+
+            Rectangle[] rectangles = { rectangle1, rectangle2, rectangle3, rectangle4 };
+
+
+            BiggestRectangle(rectangles);
+
+        }
+
+        private static void BiggestRectangle(Rectangle[] rectangles)
+        {
+            int[][] AreaRec;
+            AreaRec = new int[rectangles.Length][];
+            for (int i = 0; i < rectangles.Length; i++)
+            {
+                AreaRec[i] = [rectangles[i].CalculateArea(), i + 1];
+            }
+            var SortRectanglebyArea = AreaRec.OrderByDescending(y => y[0]);
+
+            PrintRectangleArray(SortRectanglebyArea);
+
+        }
+
+        private static void PrintRectangleArray(IOrderedEnumerable<int[]> sortRectanglebyArea)
+        {
+            var results = string.Join(Environment.NewLine, sortRectanglebyArea.Select(x => string.Join(" Area of rectangle of rectangle No : ", x)));
+            Console.WriteLine(results);
+
+        }
+    }
+}
