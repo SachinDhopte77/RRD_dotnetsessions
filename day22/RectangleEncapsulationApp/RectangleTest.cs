d1cb12d4f5f8671c6895078aea2d4726f10607ac
SachinDhopte77 <sachin.vitthaldhopte@rrd.com>
2025-01-09 12:16:21 +0530

    day22-part1

---
+++ day22/RectangleEncapsulationApp/RectangleTest.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+using RectangleEncapsulationApp.Models;
+
+namespace RectangleEncapsulationApp
+{
+    internal class RectangleTest
+    {
+        static void Main(string[] args)
+        {
+
+
+            Rectangle rectangle1 = new Rectangle();
+            rectangle1.CanYouSetWidth(100);
+            Console.WriteLine($"widht is {rectangle1.CanYouGetWidth()}");
+
+
+            rectangle1.CanYouSetHeight(-10);
+            Console.WriteLine($"height is {rectangle1.CanYouGetHeight()}");
+            Console.WriteLine("Area is " + rectangle1.CalculateArea());
+
+
+
+
+
+
+
+
+        }
+    }
+}
