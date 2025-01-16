ed1b9b08b9a7f5fa6e033dcc85b358757d8eabd8
SachinDhopte77 <sachin.vitthaldhopte@rrd.com>
2025-01-16 21:53:22 +0530

    day24

---
+++ day24/AccountConstructorExcercise/Models/Accounts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountConstructorExcercise.Models
+{
+    class Accounts
+    {
+        private readonly string _name;
+        private readonly int _accountNumber;
+        private double _accountBalance;
+        private readonly double _richectAccountbalance;
+        private readonly int _longestNamelengthlimit;
+
+        private const double richestaccountMimimumbalance = 10000;
+        private const int longestNamelengthlimit = 6;
+
+
+        public Accounts(string name, int accountNumber, double accountBalance)
+        {
+            _name = name;
+            _accountNumber = accountNumber;
+            _accountBalance = accountBalance;
+            _richectAccountbalance = richestaccountMimimumbalance;
+            _longestNamelengthlimit = longestNamelengthlimit;
+
+        }
+
+
+        public string Name { get { return _name; } }
+
+        public int AccountNumber { get { return _accountNumber; } }
+
+        public double AccountBalance { get { return _accountBalance; } }
+
+        public double RichestAccountBalanceLimit { get { return _richectAccountbalance; } }
+
+        public int NameLengthLimit { get { return _longestNamelengthlimit; } }
+
+        public void Deposit(double amount)
+        {
+
+            _accountBalance = _accountBalance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            _accountBalance = _accountBalance - amount;
+
+        }
+        public static void Transfer(Accounts account1, Accounts account2, double amount)
+        {
+            account1.Withdraw(amount);
+            account2.Deposit(amount);
+        }
+
+
+    }
+}
---
+++ day24/AccountConstructorExcercise/Program.cs
@@ -0,0 +1,61 @@
+﻿using AccountConstructorExcercise.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AccountConstructorExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Accounts sachinAccount = new Accounts("Sachin", 1001, 30000);
+            Accounts akhileshAccount = new Accounts("Akhilesh", 1002, 20000);
+            Accounts venkatAccount = new Accounts("Venkat", 1003, 9000);
+
+            Accounts[] accounts = new Accounts[] { sachinAccount, akhileshAccount, venkatAccount };
+
+            PrintRichestAccountHolderDetail(accounts);
+            PrintLongestAccountHolderNames(accounts);
+
+            int transferAmount = 400;
+            TransferFund(venkatAccount, sachinAccount, transferAmount);
+
+        }
+
+        private static void PrintRichestAccountHolderDetail(Accounts[] accounts)
+        {
+
+            var richestAccounts = accounts.Where(x => x.AccountBalance > x.RichestAccountBalanceLimit);
+            foreach (var richAccounts in richestAccounts)
+            {
+                Console.WriteLine("Richest Account Number is {0} and account balance is {1}", richAccounts.AccountNumber, richAccounts.AccountBalance);
+            }
+
+        }
+
+        private static void PrintLongestAccountHolderNames(Accounts[] accounts)
+        {
+            var lonestAccountName = accounts.Where(s => s.Name.Length > s.NameLengthLimit);
+            foreach (var longestName in lonestAccountName)
+            {
+                Console.WriteLine("Longest Name in Accountholder list is :{0}", longestName.Name);
+            }
+
+        }
+
+        private static void TransferFund(Accounts account1, Accounts account2, double transferAmount)
+        {
+            PrintDetail(account1);
+            PrintDetail(account2);
+            Accounts.Transfer(account1, account2, transferAmount);
+            PrintDetail(account1);
+            PrintDetail(account2);
+        }
+
+        private static void PrintDetail(Accounts account)
+        {
+            Console.WriteLine("Account Balance of {0} is {1}", account.Name, account.AccountBalance);
+        }
+
+    }
+}
