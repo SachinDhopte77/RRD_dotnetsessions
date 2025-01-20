981d90b9f97e40fcbeb25b5a06c5069192c2655d
SachinDhopte77 <sachin.vitthaldhopte@rrd.com>
2025-01-20 09:45:29 +0530

    day25

---
+++ day25/AccountAppUsingthisPointer/Models/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountAppUsingthisPointer.Models
+{
+    internal class Account
+    {
+        private readonly string name;
+        private readonly int accountNumber;
+        private double accountBalance;
+
+        public Account(string name, int accountNumber, double accountBalance)
+        {
+            this.name = name;
+            this.accountNumber = accountNumber;
+            this.accountBalance = accountBalance;
+        }
+
+        public string Name { get { return name; } }
+
+        public int AccountNumber { get { return accountNumber; } }
+
+        public double AccountBalance { get { return accountBalance; } }
+
+
+
+        public void Deposit(double amount)
+        {
+            accountBalance = accountBalance + amount;
+        }
+        public void Withdraw(double amount)
+        {
+            accountBalance = accountBalance - amount;
+        }
+
+        public void DoTransfer(Account account2, double transferAmount)
+        {
+            this.Withdraw(transferAmount);
+            account2.Deposit(transferAmount);
+
+        }
+    }
+}
---
+++ day25/AccountAppUsingthisPointer/Program.cs
@@ -0,0 +1,33 @@
+﻿using AccountAppUsingthisPointer.Models;
+
+namespace AccountAppUsingthisPointer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account sachinAccount = new Account("Sachin", 1001, 20000);
+            Account minuAccount = new Account("Minu", 1003, 30000);
+
+            PrintDetail(sachinAccount);
+            PrintDetail(minuAccount);
+
+            double transferAmmount = 2000;
+
+            sachinAccount.DoTransfer(minuAccount, transferAmmount);
+
+            PrintDetail(sachinAccount);
+            PrintDetail(minuAccount);
+
+
+
+        }
+
+        private static void PrintDetail(Account accountDetail)
+        {
+            Console.WriteLine($"Account Name: {accountDetail.Name}");
+            Console.WriteLine($"Account Number: {accountDetail.AccountNumber}");
+            Console.WriteLine($"Account Balance: {accountDetail.AccountBalance}");
+        }
+    }
+}
