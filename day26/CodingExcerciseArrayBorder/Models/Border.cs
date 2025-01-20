60c76dd5e9a44e7ca2b2c5a07a11f3e3920e0a41
SachinDhopte77 <sachin.vitthaldhopte@rrd.com>
2025-01-20 19:42:07 +0530

    day26

---
+++ day26/CodingExcerciseArrayBorder/Models/Border.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExcerciseArrayBorder.Models
+{
+    internal class Border
+    {
+        public static string[] AddBorderToArray(string[] arr)
+        {
+            int NumberofInputArrayValues = arr.Length;
+            int j = NumberofInputArrayValues + 2;
+            int add = 0;
+            string[] outputArray = new string[j];
+            for (int i = 0; i <= NumberofInputArrayValues - 1; i++)
+            {
+                if (i == 0)
+                {
+                    outputArray[add] = "#####";
+                    add++;
+                    outputArray[add] = "#" + arr[i] + "#";
+                }
+                else if (i == NumberofInputArrayValues - 1)
+                {
+                    add++;
+                    outputArray[add] = "#" + arr[i] + "#";
+                    add++;
+                    outputArray[add] = "#####";
+                }
+                else
+                {
+                    add++;
+                    outputArray[add] = "#" + arr[i] + "#";
+                }
+            }
+            return outputArray;
+        }
+    }
+}
---
+++ day26/CodingExcerciseArrayBorder/Program.cs
@@ -0,0 +1,23 @@
+﻿using CodingExcerciseArrayBorder.Models;
+
+namespace CodingExcerciseArrayBorder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "Sachin", "Venkat", "Akhilesh", "Minu", "Prem" };
+
+            DiplayArrayBorder(Border.AddBorderToArray(arr));
+
+        }
+
+        private static void DiplayArrayBorder(string[] outputArray)
+        {
+            foreach (string str in outputArray)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
