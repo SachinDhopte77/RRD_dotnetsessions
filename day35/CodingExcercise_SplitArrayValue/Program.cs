634bdd74d54961ce5a05a9cdb2e2f5db18f1612f
SachinDhopte77 <sachin.vitthaldhopte@rrd.com>
2025-02-02 20:19:18 +0530

    day352ed

--- day35/CodingExcercise_SplitArrayValue/Program.cs
+++ day35/CodingExcercise_SplitArrayValue/Program.cs
@@ -1,9 +1,4 @@
-﻿
-
-
-
-
-namespace CodingExcercise_SplitArrayValue
+﻿namespace CodingExcercise_SplitArrayValue
 {
     internal class Program
     {
@@ -11,67 +6,55 @@ namespace CodingExcercise_SplitArrayValue
         {
             string[] webNames = { "abc.edu", "pqr.com", "imno.org" };
 
-            string[] resultArray= RemoveDNSfromWEB(webNames);
+            string[] resultArray = RemoveDNSfromWEB(webNames);
 
             PrintResultArray(resultArray);
         }
 
-        private static void PrintResultArray(string[] resultArray)
-        {
-            foreach (string s in resultArray)
-            {  Console.WriteLine(s); }
-        }
+
 
         private static string[] RemoveDNSfromWEB(string[] webNames)
         {
-            string webNamevalue;  
+            string webNamevalue;
             int webNameslength;
             string eachStringvalue;
-            string[] outputArray= new string[webNames.Length]; //to store result array
+            string[] outputArray = new string[webNames.Length]; //to store result array
             for (int i = 0; i < webNames.Length; i++)
             {
                 webNamevalue = webNames[i];
-                webNameslength=webNamevalue.Length;// finding length of each index value
-                int dotIndexposition=FindSepratorIndex(webNamevalue,webNameslength); // to get the position of "." character
-                eachStringvalue=GenerateEachindexString(webNamevalue, dotIndexposition); // to get string before "." character
-                outputArray[i]= eachStringvalue;  // storing each index string result value
+                webNameslength = webNamevalue.Length;// finding length of each index value
+                eachStringvalue = ExtractString(webNamevalue, webNameslength); // // to get string before "." character
+                outputArray[i] = eachStringvalue;  // storing each index string result value
             }
             return outputArray;
         }
 
-        private static int  FindSepratorIndex(string webNamevalue, int webNameslength)
+        private static string ExtractString(string webNamevalue, int webNameslength)
         {
-            //functionality : to find index number of "." character
-            int dotIndexposition = 0;
-            for (int i = 0;i < webNameslength;i++)
+            //functionality : to find string before "." character
+            string currentStringValue = "";
+            string outputArraystring = "";
+            for (int i = 0; i < webNameslength; i++)
             {
-                char currentcharvalue=webNamevalue.ToCharArray()[i];
-                string currentStringValue=currentcharvalue.ToString();
+                char currentcharvalue = webNamevalue.ToCharArray()[i];
+                currentStringValue = currentcharvalue.ToString();
                 if (currentStringValue == ".")
-                { 
-                    dotIndexposition = i;                    
-                    break; 
-                }
-
-                 
-            }
-            return dotIndexposition;
-
-        }
-
-        private static string GenerateEachindexString(string webNamevalue, int dotIndexposition)
-        {
-            // Functionality : extract string before "." character
-            string outputArraystring="";
-            for (int i = 0; i < webNamevalue.Length; i++)
-            {
-                if (i < dotIndexposition)
                 {
-                    outputArraystring += webNamevalue[i].ToString();
+                    break;
                 }
-                else if (i == dotIndexposition) { break; }
+                outputArraystring += currentStringValue;
             }
             return outputArraystring;
+
         }
+        private static void PrintResultArray(string[] resultArray)
+        {
+            foreach (string s in resultArray)
+            { Console.WriteLine(s); }
+        }
+
+
     }
 }
+
+
